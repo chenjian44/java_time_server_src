e7cb74e812d91724477fef37118fbb43eb817f7f
chenjian44 <chenjian44@outlook.com>
2019-01-31 12:31:44 +0800

    net

---
+++ netty_src/TimeClient.java
@@ -0,0 +1,9 @@
+package netty_src;
+
+/**
+ * @author: 陈健
+ * @date: 2019/1/31 12:30
+ * @description:
+ */
+public class TimeClient {
+}
---
+++ netty_src/TimeServer.java
@@ -0,0 +1,57 @@
+package netty_src;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author: 陈健
+ * @date: 2019/1/31 11:57
+ * @description:
+ */
+public class TimeServer {
+
+    public void bind(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer <SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(port);
+            } catch (NumberFormatException e) {
+
+            }
+        }
+        new TimeServer().bind(port);
+
+    }
+
+}
